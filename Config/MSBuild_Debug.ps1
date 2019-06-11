cdec5db5e1875a1912b20aa1bb473142db95d34e
Brendan Hennelly <bhennelly@jaas.net>
2019-06-11 15:21:31 -0400

    MSBuild PowerShell scripts

---
+++ Config/MSBuild_Debug.ps1
@@ -0,0 +1,47 @@
+# ----------------------------------------------------------------------------------
+# script parameters
+# ----------------------------------------------------------------------------------
+$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
+$slnName = "DevConDemo.sln"
+$slnFullName = Join-Path (get-item $PSScriptRoot).parent.FullName $slnName
+$buildConfiguration = "/property:Configuration=Debug"
+
+
+
+$writeHostStartColor = 'yellow'
+$writeHostFinishColor = 'Red'
+Write-Host "==================================================================" -foregroundcolor $writeHostStartColor
+Write-Host ("=====    BEGIN: BUILDING SOLUTION {0}" -f $slnName ) -foregroundcolor $writeHostStartColor
+Write-Host "==================================================================" -foregroundcolor $writeHostStartColor
+Write-Host " "
+Write-Host $slnFullName
+Write-Host " "
+
+try
+{   
+    $buildArgs = @($slnFullName, 
+        $buildConfiguration, 
+        "/target:Rebuild")
+    & $msbuild $buildArgs
+
+    $writeHostFinishColor = 'Green'
+    Write-Host " " 
+    Write-Host " "
+    Write-Host "==================================================================" -foregroundcolor $writeHostFinishColor
+    Write-Host ("=====    END: BUILDING SOLUTION {0}" -f $slnName ) -foregroundcolor $writeHostFinishColor
+    Write-Host "==================================================================" -foregroundcolor $writeHostFinishColor
+}
+catch
+{
+    Write-Host "Error Occured While Processing..." -foregroundcolor $writeHostFinishColor
+    Write-Host $Error[0] -foregroundcolor $writeHostFinishColor
+    Write-Host " " 
+    Write-Host " "
+    Write-Host "==================================================================" -foregroundcolor $writeHostFinishColor
+    Write-Host ("=====    FAILED: BUILDING SOLUTION {0}" -f $slnName ) -foregroundcolor $writeHostFinishColor
+    Write-Host "==================================================================" -foregroundcolor $writeHostFinishColor
+}
+
+Write-Host " "
+Read-Host 'Press Enter to continue...' | Out-Null 
+
