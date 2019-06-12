8ca315833846f1809aaf1b21095e7e9a7877309f
Brendan Hennelly <bhennelly@jaas.net>
2019-06-12 09:46:27 -0400

    Updated build project for smart project description

--- Config/PXCommandLine_BuildProject.ps1
+++ Config/PXCommandLine_BuildProject.ps1
@@ -1,14 +1,36 @@
 # ----------------------------------------------------------------------------------
-# script parameters
+# BEGIN SCRIPT PARAMETERS
 # ----------------------------------------------------------------------------------
 $acuProject = "DevConDemo"
 $pxMethod = "BuildProject"
 $website = Join-Path (get-item $PSScriptRoot).parent.FullName "Site"
 $pxCommandFileName = "PX.CommandLine.exe"
 $pxCommand = Join-Path (Join-Path $website "Bin") $pxCommandFileName
+$projectDllFileName = "PX.Objects.DevConDemo.dll"
 $acuProjectRoot = Join-Path (get-item $PSScriptRoot).parent.FullName ("AcumaticaProjects\{0}" -f $acuProject)
 $acuProjectOutput = Join-Path (get-item $PSScriptRoot).parent.FullName ("AcumaticaProjects\Install\{0}.zip" -f $acuProject)
 $taskName = ("Acumatica Command - {0} - project '{1}'" -f $pxMethod, $acuProject)
+$projectDesc = "DevConDemo - 0.0.0.0 - 2019.01.01"
+# ----------------------------------------------------------------------------------
+# END SCRIPT PARAMETERS
+# ----------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------
+# BEGIN FUNCTIONS
+# ----------------------------------------------------------------------------------
+Function Get-ProjectVersionDate($dllFile, $dateFormat)
+{
+    $versionInfo = (ls $dllFile -r | % versioninfo)
+    $fileDate = (Get-ChildItem $dllFile).LastWriteTime
+    $FormatedFileDate = $fileDate.ToString($dateFormat)
+
+    return ("{0} - {1}" -f $versionInfo.FileVersion, $FormatedFileDate )
+}
+
+# ----------------------------------------------------------------------------------
+# END FUNCTIONS
+# ----------------------------------------------------------------------------------
+
 
 $writeHostStartColor = 'yellow'
 $writeHostFinishColor = 'Red'
@@ -19,7 +41,16 @@ Write-Host " "
 
 try
 {   
-    Invoke-Expression ("$pxCommand /method `"{0}`" /website `"{1}`" /in `"{2}`" /out `"{3}`" /level `"0`" /description `"{4}`"" -f $pxMethod, $website, $acuProjectRoot, $acuProjectOutput, "TestDesc")
+    # Build a smart project description using project name, dll version, and date (time) of dll
+    $projectDll = Join-Path (Join-Path $acuProjectRoot "Bin") $dllFileName
+    $projectVersionDate = Get-ProjectVersionDate $projectDll "yyyy.MM.dd (HH:mm)"
+    $projectDesc = ("{0} - {1}" -f $acuProject, $projectVersionDate )
+
+    $exp = "$pxCommand /method `"$pxMethod`" /website `"$website`" /in `"$acuProjectRoot`" /out `"$acuProjectOutput`" /level `"0`" /description `"$projectDesc`""
+    Write-Host $exp
+    Write-Host " "
+
+    Invoke-Expression $exp
 
     $writeHostFinishColor = 'Green'
     Write-Host " " 
