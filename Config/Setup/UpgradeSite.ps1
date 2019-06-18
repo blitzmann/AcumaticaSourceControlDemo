14f4e9eaf0e04d0ebe389eb11c6987032e13acb6
Brendan Hennelly <bhennelly@jaas.net>
2019-06-18 15:09:38 -0400

    update setup scripts to use a paramter file

--- Config/Setup/UpgradeSite.ps1
+++ Config/Setup/UpgradeSite.ps1
@@ -2,7 +2,7 @@
 #
 #   DevConDemo file
 #
-#   Updated 06/17/2019
+#   Updated 06/18/2019
 #
 # **********************************************************************************
 
@@ -25,7 +25,149 @@ $newProcess.Verb = 'runas';
 exit
 }
 
+# --- Parameters
+[string]$scriptPath = $PSScriptRoot
+$scriptBranchRoot = (get-item $scriptPath ).parent.parent.FullName
+$scriptBranchName = (get-item $scriptPath ).parent.parent.Name
+$scriptParmFile = Join-Path $scriptPath "SiteParameters.txt"
+$sitePath = Join-Path $scriptBranchRoot 'Site'
+$erpPath = Join-Path $scriptBranchRoot 'ERP'
+
+#paramter file parameters (setting defaults before file value)
+$script:paramInstanceName = ("DEV{0}" -f $scriptBranchName)
+$script:paramDatabaseName = ("DEV{0}" -f $scriptBranchName)
+[bool]$script:paramIsNewDatabase = $true
+[bool]$script:paramInsertDemoData = $true
+$script:paramAcumaticaERPInstallDirectory = "C:\Program Files\Acumatica ERP"
+
 # ... Begin functions
+
+function Write-ImportantInfo($msg)
+{
+    Write-Host ("")
+    Write-Host ("** {0}" -f $msg) -foregroundcolor Black -BackgroundColor Yellow
+    Write-Host ("")
+}
+
+function Create-ParameterFile ()
+{
+    if ((Test-Path $scriptParmFile) -eq $false)
+    {
+        $instance = $paramInstanceName
+        $dbName = Get-DatabaseName $sitePath
+        if([string]::IsNullOrWhiteSpace($dbName)) {
+            $dbName = $paramDatabaseName
+        }
+        else {
+            $instance = $dbName
+            $paramIsNewDatabase = $false
+            $paramInsertDemoData = $false
+        }
+
+        $private:buffer = @()
+        $buffer += ("InstanceName={0}" -f $instance)
+        $buffer += ("DatabaseName={0}" -f $dbName)
+        $buffer += ("IsNewDatabase={0}" -f $paramIsNewDatabase)
+        $buffer += ("InsertDemoData={0}" -f $paramInsertDemoData)
+        $buffer += ("AcumaticaERPInstallDirectory={0}" -f $paramAcumaticaERPInstallDirectory)
+        
+        Write-ImportantInfo ('Creating File: {0}' -f $scriptParmFile)
+        $buffer | Out-File $scriptParmFile -Encoding Default
+    }
+}
+
+function Get-Parameters ()
+{
+    Create-ParameterFile
+
+    if ((Test-Path $scriptParmFile) -ne $false)
+    {
+        $foundError = $false
+        $fileContent = Get-Content $scriptParmFile
+        Write-Host "*************************************************************"
+        Write-Host "Input parameters:"
+	    foreach ($line in $fileContent)
+        {
+            Write-Host $line
+            $line = $line.split("=")
+            if($line.Count -eq 2)
+            {
+                Set-Parameter $line[0] $line[1]
+            }
+            else
+            {
+                $foundError = $true
+                Write-Host "Bad Script Parameters file content: {0}" -f $line
+            }
+        }
+        Write-Host "*************************************************************"
+
+        if($foundError -eq  $true)
+        {
+            throw "Error getting parameters"
+        }
+    }  
+    
+    # Use what the web config has for the database - only use the db from the param file when new site
+    $script:paramDatabaseName = Get-DatabaseName $sitePath
+}
+
+function Set-Parameter($pKey, $pValue)
+{
+    switch ($pKey)
+    {
+        "InstanceName" { $script:paramInstanceName = $pValue; break}
+        "DatabaseName" { $script:paramDatabaseName = $pValue; break}
+        "IsNewDatabase" { $script:paramIsNewDatabase =  Get-ParameterBoolValue $pValue; break}
+        "InsertDemoData" { $script:paramInsertDemoData = Get-ParameterBoolValue $pValue; break}
+        "AcumaticaERPInstallDirectory" { $script:paramAcumaticaERPInstallDirectory = $pValue; break}
+        Default { throw ("Invalid parameter key {0}" -f $pKey)  }
+    }
+}
+
+#condition parameter values in case not bool entered value
+function Get-ParameterBoolValue ($pValue)
+{
+    if($pValue -eq "true" -or $pValue -eq "yes")
+    {
+        return $true
+    }
+
+    return $false
+}
+
+function Check-Paramters()
+{
+    $errorFound = $false
+    if([string]::IsNullOrWhiteSpace($paramAcumaticaERPInstallDirectory) -or !(Test-Path $paramAcumaticaERPInstallDirectory))
+    {
+        $errorFound = $true
+        Write-ImportantInfo ("Invalid Acumatica ERP Install Directory: {0}" -f $paramAcumaticaERPInstallDirectory)
+    }
+
+    if ([string]::IsNullOrWhiteSpace($paramInstanceName))
+    {
+        $errorFound = $true
+        Write-ImportantInfo ("InstanceName cannot be empty")
+    }
+
+    if ([string]::IsNullOrWhiteSpace($paramDatabaseName))
+    {
+        $errorFound = $true
+        Write-ImportantInfo ("DatabaseName cannot be empty")
+    }
+
+    if($script:paramIsNewDatabase -eq $false -and $script:paramInsertDemoData -eq $true)
+    {
+        # not inserting demo data into an existing database
+        $script:paramInsertDemoData = $false
+    }
+
+    if($errorFound -eq $true)
+    {
+        throw "Errors in parameters."
+    }
+}
 
 # Given a web.config file path, return the Acumatica version
 Function Get-WebConfigVersion([string]$xmlFile)
@@ -55,25 +197,7 @@ Function Get-WebAppVersion([xml]$xmlContent)
 
 function Get-CommandToolPath()
 {
-    $commandTool = Join-Path $scriptPath 'Data'
-    if(Test-Path $commandTool)
-    {
-        return $commandTool
-    }
-
-    $commandTool = Join-Path $scriptPath 'Acumatica ERP\Data'
-    if(Test-Path $commandTool)
-    {
-        return $commandTool
-    }
-
-    $commandTool = Join-Path 'C:\Program Files\Acumatica ERP' 'Data'
-    if(Test-Path $commandTool)
-    {
-        return $commandTool
-    }
-
-    $commandTool = Join-Path 'C:\Program Files (x86)\Acumatica ERP' 'Data'
+    $commandTool = Join-Path $script:paramAcumaticaERPInstallDirectory 'Data'
     if(Test-Path $commandTool)
     {
         return $commandTool
@@ -200,7 +324,10 @@ Function CleanupWebConfig($sitePath, $erpPath)
 
     if([System.IO.File]::Exists($webConfigPath) -eq $false)
     {
-        Write-Error ("Cannot find web.config files; {0}" -f $webConfigPath)
+        Write-Host ""
+        Write-Warning ("Cannot find web.config file: {0}" -f $webConfigPath)
+        Write-Warning "Unable to cleanup Web.Config"
+        Write-Host ""
         return
     }
 
@@ -216,20 +343,31 @@ Function CleanupWebConfig($sitePath, $erpPath)
         New-Item $erpPath -type directory | Out-Null
     }
 
-    [string]$snapshotsFolder = MoveCreateSiteTempFolder $erpPath $sitePathParent "Snapshots"
-    [string]$customizationTempFolder = MoveCreateSiteTempFolder $erpPath $sitePathParent "Customization"
-    [string]$backupFolder = MoveCreateSiteTempFolder $erpPath $sitePathParent "Backup"
-    [string]$tempDirectory = MoveCreateSiteTempFolder $erpPath $sitePathParent "TemporaryAspFiles"
+    # to seperate Site and Portal we want a sub folder in the ERP folder
+    $siteErpPath = Join-Path $erpPath 'Site'
+    if(!(Test-Path $siteErpPath))
+    {
+        Write-Host ('Creating ERP Folder: {0}' -f $siteErpPath)
+        New-Item $siteErpPath -type directory | Out-Null
+    }
 
-    # make sure debug set to true
-    $node2 = $xml.configuration.'system.web'.compilation
-    $node2.debug = "True"
+    [string]$snapshotsFolder = MoveCreateSiteTempFolder $siteErpPath $sitePathParent "Snapshots"
+    [string]$customizationTempFolder = MoveCreateSiteTempFolder $siteErpPath $sitePathParent "Customization"
+    [string]$backupFolder = MoveCreateSiteTempFolder $siteErpPath $sitePathParent "Backup"
+    
+    $compilationNode = [System.Xml.XmlNode]$xml.configuration.'system.web'.compilation
+    $compilationNode.debug = "True"
 
-    #this might be new to 19R1 or some recent version
-    $node2.tempDirectory = $tempDirectory
+    # new in 19R1+
+    if([string]::IsNullOrWhiteSpace($compilationNode.Attributes["tempDirectory"]) -eq $false)
+    {
+        [string]$tempDirectory = MoveCreateSiteTempFolder $siteErpPath $sitePathParent "TemporaryAspFiles"
+        $compilationNode.tempDirectory = $tempDirectory
+    }
 
     $nodeCustomizationPaths = $xml.selectNodes('//configuration//appSettings//add')
     foreach ($path in $nodeCustomizationPaths) {
+
       if($path.key -eq "CustomizationTempFilesPath"){
         $path.value = $customizationTempFolder
       }
@@ -275,13 +413,11 @@ function Upgrade-AcumaticaSite([string]$siteVirtualDirectoryName, [string]$datab
     $sitePath = ('-ipath:"{0}"' -f $acuSitePath)
     $virtualDirName = ('-svirtdir:"{0}"' -f $siteVirtualDirectoryName)
     $appPool = '-spool:"DefaultAppPool"'
-    $salesDemo = ''
-    $company = ('-company:"CompanyID=2;ParentID=1;Visible=True;CompanyType={0};LoginName=TestCompany;Delete=False"' -f $salesDemo)
 
     Write-Host ""
     Write-Host ("Running {0} using {1}" -f $CMD, $configMode) -ForegroundColor Yellow
     Write-Host ""
-    Write-Host $CMD $configMode $dbNew $dbWinAuth $dbServerName $dbName $dbConnWinAuth $sitePath $company $appPool $localWebsite $portalSite $virtualDirName
+    Write-Host $CMD $configMode $dbNew $dbWinAuth $dbServerName $dbName $dbConnWinAuth $sitePath $appPool $localWebsite $portalSite $virtualDirName
     Write-Host ""
 
     & $CMD $configMode $dbNew $dbWinAuth $dbServerName $dbName $dbConnWinAuth $sitePath $company $appPool $localWebsite $portalSite $virtualDirName
@@ -318,40 +454,43 @@ function Upgrade-AcumaticaSite([string]$siteVirtualDirectoryName, [string]$datab
 
 Try
 {
-    [string]$scriptPath = $PSScriptRoot
-    $sitePath = Join-Path (get-item $scriptPath ).parent.parent.FullName 'Site'
-    $erpPath = Join-Path (get-item $scriptPath ).parent.parent.FullName 'ERP'
-    $webConfigPath = Join-Path $sitePath 'Web.Config'
-    $commandToolPath = Get-CommandToolPath
-
-    if([string]::IsNullOrWhiteSpace($commandToolPath))
+    if ((Test-Path $scriptParmFile) -eq $false)
     {
-        $commandToolPath = "D:\AcumaticaInstalls\19.105.0032\Acumatica ERP\Data"
-        #$commandToolPath = Read-Host -Prompt "Unable to find ERP Data folder. Enter ERP Data folder location. (Ex: 'C:\Program Files\Acumatica ERP')"
+        Create-ParameterFile
+
+        Write-Host " "
+        Write-Host "<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>" -ForegroundColor Cyan
+        Write-Host "Created supporting file(s). Please review values and run the script again" -ForegroundColor Yellow
+        Write-Host "<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>" -ForegroundColor Cyan
+        Write-Host " "
+        # we want the paramter file to be created giving the user an oportunity to replace the defaults before running
+        # this will skip to the finnally statement
+        return
     }
 
-    if((Test-ValidPath $commandToolPath) -eq $false)
+    Get-Parameters
+    Check-Paramters
+    
+    $commandToolPath = Get-CommandToolPath
+    if([string]::IsNullOrWhiteSpace($commandToolPath) -or (Test-ValidPath $commandToolPath) -eq $false)
     {
-        throw "Unable to find Acumatica ERP Data directory"
+        throw ("Unable to find Acumatica ERP Data directory in '{0}'" -f $commandToolPath)
     }
 
     [string]$version = Get-AcumaticaVersion $commandToolPath
     $writeHostColor = 'yellow'
 
+    Write-Host ""
+    Write-Host ("Using Acumatica configuration version {0}" -f $version) -foregroundcolor Cyan
+    Write-Host ""
+
+    $webConfigPath = Join-Path $sitePath 'Web.Config'
     if([System.IO.File]::Exists($webConfigPath) -eq $false)
     {
         throw ("Missing file {0}" -f $webConfigPath)
     }
 
     $webVersion = Get-WebConfigVersion $webConfigPath
-
-    Write-Host ""
-    Write-Host ("Using Acumatica configuration version {0}. Upgrading from {1}" -f $version, $webVersion) -foregroundcolor Yellow
-    Write-Host ""
-
-    $instanceName = "DevConDemo"
-    $dbName = Get-DatabaseName $sitePath
-    [bool]$isPortal = $false
 
     # If the site already exists the prompt will ask this question to continue:
     #   Acumatica CW:
@@ -361,11 +500,11 @@ Try
     if([System.IO.File]::Exists((Join-Path $sitePath 'Bin\PX.Objects.dll')) -eq $true)
     {
         # move current site folder to a backup location to create a new site. The AC.exe does not like files existing in the path and prompts for continue but not sure how to answer the prompt via command line
-        $siteBackupDir = Join-Path $erpPath 'LocalSitesBackup'
+        $siteBackupDir = Join-Path $erpPath 'ScriptBackup'
         Create-Directory $siteBackupDir
         $now = Get-Date -Format FileDateTime
         $siteTimeStamp = ("Site_{0}" -f $now)
-        if(![string]::IsNullOrWhiteSpace($commandToolPath))
+        if(![string]::IsNullOrWhiteSpace($webVersion))
         {
             $siteTimeStamp = ("Site_{0}_{1}" -f $webVersion, $now)
         }
@@ -374,7 +513,8 @@ Try
         Copy-Item $sitePath -Destination $destinationSitePath -Recurse  | Out-Null
     }
 
-    Upgrade-AcumaticaSite $instanceName $dbName $isPortal $sitePath $commandToolPath
+    [bool]$isPortal = $false
+    Upgrade-AcumaticaSite $paramInstanceName $paramDatabaseName $isPortal $sitePath $commandToolPath
 
     CleanupWebConfig $sitePath $erpPath
 }
