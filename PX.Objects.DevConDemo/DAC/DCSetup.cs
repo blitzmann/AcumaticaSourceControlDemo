ff5acffe93ed8a2f68159f8708d6106457b2b113
Brendan Hennelly <bhennelly@jaas.net>
2019-06-18 17:20:46 -0400

    Customization Plugin test

---
+++ PX.Objects.DevConDemo/DAC/DCSetup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PX.Data;
+
+namespace PX.Objects.DevConDemo
+{
+    [Serializable]
+    public class DCSetup : IBqlTable
+    {
+        #region PluginOnPublished
+        [PXDBString(500, IsUnicode = true, InputMask = "")]
+        [PXUIField(DisplayName = "On Published Msg")]
+        public virtual string PluginOnPublished { get; set; }
+        public abstract class pluginOnPublished : PX.Data.BQL.BqlString.Field<pluginOnPublished> { }
+        #endregion
+
+        #region PluginUpdateDatabase
+        [PXDBString(500, IsUnicode = true, InputMask = "")]
+        [PXUIField(DisplayName = "Update Database Msg")]
+        public virtual string PluginUpdateDatabase { get; set; }
+        public abstract class pluginUpdateDatabase : PX.Data.BQL.BqlString.Field<pluginUpdateDatabase> { }
+        #endregion
+    }
+}
---
+++ PX.Objects.DevConDemo/PluginTest.cs
@@ -0,0 +1,39 @@
+﻿using Customization;
+using PX.Data;
+
+namespace PX.Objects.DevConDemo
+{
+    //The customization plug-in is used to execute custom actions after the customization project has been published
+    // To Add a Customization Plug-In to a Project:
+    //      https://help-2019r1.acumatica.com/?ScreenId=ShowWiki&pageid=c69443fe-4d32-47a9-85aa-b2882aa259ef
+    public class PluginTest : CustomizationPlugin
+    {
+        protected string TimeStampString => PX.Common.PXTimeZoneInfo.Now.ToString("yy-MM-dd hh:mm:ss");
+
+        //This method is executed right after website files are updated, but before the website is restarted
+        //The method is invoked on each cluster node in a cluster environment
+        //The method is invoked only if runtime compilation is enabled
+        //Do not access custom code published to bin folder; it may not be loaded yet
+        public override void OnPublished()
+        {
+            InsertUpdateSetup<DCSetup.pluginOnPublished>($"OnPublished;{TimeStampString}");
+        }
+
+        //This method is executed after the customization has been published and the website is restarted.
+        public override void UpdateDatabase()
+        {
+            InsertUpdateSetup<DCSetup.pluginUpdateDatabase>($"UpdateDatabase;{TimeStampString}");
+        }
+
+        protected void InsertUpdateSetup<Field>(string msg)
+            where Field : IBqlField
+        {
+            WriteLog(msg);
+            if (!PXDatabase.Update<DCSetup>(new PXDataFieldAssign<Field>($"{msg};UPDATE")))
+            {
+                //Insert...
+                PXDatabase.Insert<DCSetup>(new PXDataFieldAssign<Field>($"{msg};INSERT"));
+            }
+        }
+    }
+}
